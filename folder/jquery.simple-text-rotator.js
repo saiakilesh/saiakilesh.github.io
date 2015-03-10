699cad00ff7434ea45c02a765719a5c50cd2efc7
Akilesh Tangella <saiakilesh@gmail.com>
2015-03-09 23:04:07 -0400

    Update jquery.simple-text-rotator.js

--- folder/jquery.simple-text-rotator.js
+++ folder/jquery.simple-text-rotator.js
@@ -165,10 +165,4 @@
   
 }(window.jQuery);
 
-$(".rotate").textrotator({
-  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
-  separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
-  speed: 2000 // How many milliseconds until the next word show.
-});
-
 
